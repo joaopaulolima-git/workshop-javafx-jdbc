f30c43e0d193042784284b9038790e159cc5ea19
João Paulo <jplopapa.git@gmail.com>
2021-08-07 17:46:43 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -69,6 +72,9 @@ public class DepartmentFormController implements Initializable {
 			notifyDataChangeListener();
 			Utils.currentStage(event).close();
 		}
+		catch (ValidationException e) {
+			setErrosMessage(e.getErros());
+		}
 		catch (DbException e) {
 			Alerts.showAlerts("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -83,8 +89,16 @@ public class DepartmentFormController implements Initializable {
 	private Department getFromData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation erro");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErros("name", "O campo não pode estar vazio");
+		}
 		obj.setName(txtName.getText());
+		if (exception.getErros().size() > 0) {
+			throw exception;
+		}
 		return obj;
 	}
 
@@ -109,5 +123,13 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrosMessage(Map<String, String> erros) {
+		Set<String> Fields = erros.keySet();
+		
+		if (Fields.contains("name")) {
+			LabelErrorName.setText(erros.get("name"));
+		}
 	}
 }
